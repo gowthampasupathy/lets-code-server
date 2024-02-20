60431de3748a67662aa09aa42dbc9cb1c7b095d2
gowthampasupathy <140435319+gowthampasupathy@users.noreply.github.com>
2024-02-20 10:47:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.post("/login",verifyUser,verifyadmin,(req,res)=>{
             bcrypt.compare(password,result.password,(err,resp)=>{
                 if(resp){
                     const tkn=jwt.sign({email:result.email,role:result.role},"jwt-secret-key",{expiresIn:'1d'})
-                    res.cookie('token',tkn,{ httpOnly: true,secure:true,sameSite: 'strict'})
+                    res.cookie('token',tkn,{ httpOnly: true,secure:true,strict: 'none'})
                     return res.json({status:"Success",role:result.role})
 
                 }else{
