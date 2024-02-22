1b7e8b480b7332fd98547ca900a5dfe7b28b6d99
gowthampasupathy <gowthampasupathi17@gmail.com>
2024-02-22 09:26:34 +0530

    0.0.0.1

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.post("/login",(req,res)=>{
             bcrypt.compare(password,result.password,(err,resp)=>{
                 if(resp){
                     const token=jwt.sign({email:result.email,role:result.role},"jwt-secret-key",{expiresIn:'1d'})
-                    res.cookie('token',token,{ httpOnly: true })
+                    res.cookie('token',token)
                     console.log("cookies set succedd")
                     return res.json({status:"Success",role:result.role})
 
