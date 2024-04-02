efc3f058c7d025a78c6f937d6f49918c981bbf87
gowthampasupathy <gowthampasupathi17@gmail.com>
2024-04-03 02:14:37 +0530

    0.0.0.1

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const usertrackmodal=require('./modals/usertrack')
 
 const app=express()
 app.use(cors({
-    origin:["https://lets-code-api.onrender.com"],
+    origin:["https://lets-code-self.vercel.app"],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true
 }))
